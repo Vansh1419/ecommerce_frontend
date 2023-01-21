e3c508f7c71a5ac536c732bc26f65e7d4c73eea0
Rohit Gupta <76804979+lucidrohit@users.noreply.github.com>
2023-01-21 23:31:07 +0530

    removed unwanted code from addressbar

--- frontend/src/components/NavBar/NavBarComponents/AddressBar/AddressBar.jsx
+++ frontend/src/components/NavBar/NavBarComponents/AddressBar/AddressBar.jsx
@@ -1,52 +1,26 @@
 import React from "react";
-import { Grid, Typography } from "@mui/material";
+import { Typography,Box } from "@mui/material";
 import { KeyboardArrowDown } from "@mui/icons-material";
 import Dropdown from "../../../common/Dropdown/Dropdown";
 
 const AddressBar = () => {
   return (
-    <div>
-      <Grid
-        container
-        className="address__container"
-        sx={{
-          justifyContent: "center",
-          alignItems: "center",
-          px: {
-            xl: 2,
-            xs: 0,
-          },
-          height: {
-            xl: "86px",
-            xs: "43px",
-          },
-          mixBlendMode: "multiply",
-          mx: 2,
-          "&:hover": {
-            background: "#f5f5f5",
-          },
+    <Box display="flex" alignItems="center" flexGrow={1} justifyContent="center">
+      <Typography whiteSpace="nowrap" variant="subtitle2">Delivery at</Typography>
+      <Dropdown
+        info={{
+          option_name: "Current Address",
+          option_icon: <KeyboardArrowDown />,
+          options: [
+            "address 1",
+            "address 2",
+            "address 3",
+            "address 4",
+            "address 5",
+          ],
         }}
-      >
-        <Grid item xs={5} sm={3} xl={12}>
-          <Typography variant="p">Delivery at</Typography>
-        </Grid>
-        <Grid item xs={7} sm={9} xl={12}>
-          <Dropdown
-            info={{
-              option_name: "Current address",
-              option_icon: <KeyboardArrowDown />,
-              options: [
-                "address 1",
-                "address 2",
-                "address 3",
-                "address 4",
-                "address 5",
-              ],
-            }}
-          />
-        </Grid>
-      </Grid>
-    </div>
+      />
+    </Box>
   );
 };
 
