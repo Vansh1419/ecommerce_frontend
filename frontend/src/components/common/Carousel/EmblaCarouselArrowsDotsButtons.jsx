6a3a987e70d53870a5462b18f0088645d44d6e37
Rohit Gupta <76804979+lucidrohit@users.noreply.github.com>
2023-01-21 17:55:20 +0530

    did typo correction from class to className

--- frontend/src/components/common/Carousel/EmblaCarouselArrowsDotsButtons.jsx
+++ frontend/src/components/common/Carousel/EmblaCarouselArrowsDotsButtons.jsx
@@ -21,10 +21,10 @@ export const PrevButton = (props) => {
       onClick={onClick}
       disabled={!enabled}
     >
-      <svg class="flickity-button-icon" viewBox="0 0 100 100">
+      <svg className="flickity-button-icon" viewBox="0 0 100 100">
         <path
           d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z"
-          class="arrow"
+          className="arrow"
         ></path>
       </svg>
     </button>
@@ -40,10 +40,10 @@ export const NextButton = (props) => {
       onClick={onClick}
       disabled={!enabled}
     >
-      <svg class="flickity-button-icon" viewBox="0 0 100 100">
+      <svg className="flickity-button-icon" viewBox="0 0 100 100">
         <path
           d="M 10,50 L 60,100 L 70,90 L 30,50  L 70,10 L 60,0 Z"
-          class="arrow"
+          className="arrow"
           transform="translate(100, 100) rotate(180) "
         ></path>
       </svg>
